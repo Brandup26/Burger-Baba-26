2b936e8d8fd660c73b5a956684b8f0334e9efdfe
Brandup26 <anasgaber504@gmail.com>
2026-05-25 22:36:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  console.log('Burger Baba Service Worker Installed');
+});
+self.addEventListener('fetch', (e) => {
+  // تفعيل جلب البيانات العادي للمنيو المتغير
+});
